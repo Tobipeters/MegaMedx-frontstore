2001a8b7eb6270b8dce85aa2b5741024b94402ec
Tobipeters <oluwatobilobapeterschem@gmail.com>
2020-08-12 19:39:13 +0100

    additional changes on today's task

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
 //script for the time left in deals of the day 
-
-//starting timer conditionally 
-// var timerInterval = null;
-// if (!timerInterval) {
-const outTimeLeft = () => {
-    var deadline = new Date('sat aug 15 2020 11:00:08').getTime()
-    var now = new Date().getTime()
-    var timeLeft = deadline - now
+var alertDiv = document.getElementById("time-left")
+//console.log(alertDiv)
+const x = () => {
     setInterval(function () {
+        var deadline = new Date('Sun aug 15  2020 11:00:08').getTime()
+        var now = new Date().getTime()
+        var timeLeft = deadline - now
         var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
         var hours = Math.floor(timeLeft % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
         var mins = Math.floor(timeLeft % (1000 * 60 * 60) / (1000 * 60));
         var secs = Math.floor(timeLeft % (1000 * 60) / 1000);
-        console.log(`${days} days ${hours} hours ${mins} mins ${secs} secs`);
-
+        //console.log(`${days} Days ${hours} Hours ${mins} Mins ${secs} Secs `);
         //accessing DOM of time left alert 
-        document.getElementById("time-left").innerHTML = `${days} Days ${hours} Hours ${mins} Mins ${secs} Secs`
+        alertDiv.innerHTML = `${days} Days ${hours} Hours ${mins} Mins ${secs} Secs `
+        if (timeLeft < 0) {
+            //to stop the timer
+            clearInterval(x);
+            alertDiv.innerHTML = "EXPIRED";
+        }
     }, 1000);
 }
-outTimeLeft();
-// }
+
+x();
+//Stepper functionalities for Prime vendor sign up
+var secondStep = document.getElementById('second-step')
+var secondStepDiv = document.getElementById('step-two-div')
+var firstStepDiv = document.getElementById("step-one-div")
+//business information
+displayPersonContact = () => {
+    secondStep.className += " active";
+    firstStepDiv.style.display = "none"
+    secondStepDiv.style.display = "block"
+    // console.log(secondStepDiv)
+}
+displayBusinessInfo = () => {
+    secondStep.classList.remove("active")
+    secondStepDiv.style.display = "none"
+    firstStepDiv.style.display = "block"
+
+    // console.log(secondStepDiv)
+}
+
